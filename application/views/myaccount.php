009b2699a302dbe96b991affd0e0c55b9bb01f09
chetanakum007 <49388456+chetanakum007@users.noreply.github.com>
2019-05-04 00:57:18 +0530

    html layout setup

---
+++ application/views/myaccount.php
@@ -0,0 +1,42 @@
+<main>
+	<div class="container account-page">
+		<div class="row justify-content-center my-5 py-5">
+			<aside class="col-md-3 mb-4 mb-md-0">
+				<div class="card p-0 border-0 bg-transparent">
+					<h3 class="mb-4">Manage my account</h3>
+					<div class="card-body p-0 border-0">
+						<ul class="list-group">
+							<li class="list-group-item border-0 active"><a href="my-account.html"><i class="fas fa-globe"></i>My Account</a></li>
+							<li class="list-group-item border-0"><a href="my-bis-pro.html"><i class="fas fa-user"></i>My Business Profile</a></li>
+							<li class="list-group-item border-0"><a href="#"><i class="fas fa-lock"></i>Password and Security</a></li>
+						</ul>
+					</div>
+					</div>
+			</aside>
+			<div class="col-md-7 shadow bg-white px-0">
+				<div class="mb-3 card border-0">
+					<div class="card-header bg-transparent">
+						<h5 class="position-relative mb-0">My Account<a href="my-edit.html"><i class="fas fa-pencil-alt"></i></a></h5>
+					</div>
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-3">
+								<img src="<?= base_url(); ?>assets/images/apic.jpg" title="apic" alt="apic" class="img-res">
+							</div>
+							<div class="col-md-9 account-content-block">
+								<h5 class="mt-0">Jonathan Mc Mahon</h5>
+								<div class="acc-mail">
+									<p class="d-inline-block">jmcmahon@creditrescue.com</p><span class="ml-2 verified-span"><i class="fas fa-check-circle mx-1"></i>verified</span>
+								</div>
+								<div class="acc-mail-content">
+									<span class="d-block"><i class="fas fa-check-circle mr-2"></i>Offers credit repair services</span>
+									<span class="d-block"><i class="fas fa-times-circle mr-2"></i>Does not offer services to the industry</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
