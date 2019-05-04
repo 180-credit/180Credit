6ca6299fc5c39781162821e8321cc2be42452eed
chetanakum007 <chetanakum007@gmail.com>
2019-05-04 13:40:47 +0530

    html layout setup

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -4,6 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_model extends CI_Model {
 
+	public function test() {
+		echo "test";
+	}
     public function getLogin($email, $pwd) {
 		return $this->db->query("select * from ".$this->common->getUserTable()." WHERE email_id='".$email."' AND password='".$pwd."' AND active=1")->row();
     }
