05097b198f76a582f13caf1b428a295f5d58821a
hiren suhagiya <hirensuhagiya11@gmail.com>
2019-05-07 12:11:20 +0530

    changes in db

--- database/dbchanges.sql
+++ database/dbchanges.sql
@@ -1,2 +1,3 @@
 -- 6/5/19
 ALTER TABLE `users` ADD `profile_image` TEXT NULL AFTER `verificationToken`;
+ALTER TABLE `users` ADD `isCreditRepairService` BOOLEAN NOT NULL DEFAULT FALSE AFTER `profile_image`, ADD `isCreditRepairIndustry` BOOLEAN NOT NULL DEFAULT FALSE AFTER `isCreditRepairService`;
