2eeaf56a9c8ea7307d24c61f71ea1570888209da
chetanakum007 <chetanakum007@gmail.com>
2019-06-12 13:46:43 +0530

    chat config

--- chat/ChatApp/serverConf.js
+++ chat/ChatApp/serverConf.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 let app={};
 
-const SSL=false;   // true if ssl active and protocol HTTPS
+const SSL=true;   // true if ssl active and protocol HTTPS
 app.ID_LOGIN=false;
 
 app.createServer=function (app) {
@@ -10,9 +10,9 @@ app.createServer=function (app) {
         const fs=require("fs");
         // certificate paths
         const opt={
-            key: fs.readFileSync('/etc/privkey.pem'), //location of private key file.required. extension can be .key
-            cert: fs.readFileSync('/etc/cert.pem'), //location of certificate file.required. extension can be .cert
-            ca: fs.readFileSync('/etc/chain.pem') // optional
+            key: fs.readFileSync('/home/credit/ssl/keys/afced_4c51f_bd476b75bba1d99a832cd357dce2329f.key'), //location of private key file.required. extension can be .key
+            cert: fs.readFileSync('/home/credit/ssl/certs/180credit_com_afced_4c51f_1563321599_d3c7cf9818659f4dd9840ef730f959dd.cr'), //location of certificate file.required. extension can be .cert
+            // ca: fs.readFileSync('/etc/chain.pem') // optional
         };
         server = require("https").createServer(opt,app); // https port 8443
     }else{
@@ -24,7 +24,7 @@ app.createServer=function (app) {
 app.startServer=function (server) {
     // https port 8443
     if(SSL){
-        server.listen(process.env.PORT || 8443, function () {
+        server.listen(process.env.PORT || 60000, function () {
             let host = server.address().address;
             let port = server.address().port;
             console.log("\n\n---------------------------- " + moment().format('MMMM Do YYYY, hh:mm:ss') + " ----------------------------\n");
